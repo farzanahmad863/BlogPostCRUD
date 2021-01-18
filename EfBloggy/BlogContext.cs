c081a305013ba770e00accb5469380da93470cb7
farzanahmad863 <farzanahmad863@yahoo.com>
2021-01-18 10:59:23 +0100

    Add project files.

---
+++ EfBloggy/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfBloggy
+{
+    public class BlogContext : DbContext
+    {
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+              "Data Source=DESKTOP-KF048ET;Initial Catalog=EFBloggy;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
+
